396504696ff6f6bd41e33f3241ec9d30f2e95fb3
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-31 15:57:00 +0700

    fix error

--- src/main/java/com/keycloakspringboot/controller/KeycloakController.java
+++ src/main/java/com/keycloakspringboot/controller/KeycloakController.java
@@ -2,22 +2,28 @@ package com.keycloakspringboot.controller;
 
 import java.security.Principal;
 
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
-import org.keycloak.representations.AccessToken;
+import javax.servlet.http.HttpServletRequest;
+
+// import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+// import org.keycloak.representations.AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KeycloakController {
-    @GetMapping("/login")
-    public String login() {
-        return "Welcome to login screen";
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws Exception {
+        request.logout();
+        return "redirect:/";
     }
 
     @GetMapping("/homepage")
     public String homepage(Principal principal) {
-        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) principal;
-        AccessToken accessToken = token.getAccount().getKeycloakSecurityContext().getToken();
-        return "Welcome to homepage, " + accessToken.getPreferredUsername() + " successfully logged in";
+        // KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) principal;
+        // AccessToken accessToken =
+        // token.getAccount().getKeycloakSecurityContext().getToken();
+
+        return "Welcome to homepage, " + principal.getName() + " successfully logged in";
     }
 }
